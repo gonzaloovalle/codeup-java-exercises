6ab61590e8eb2129bf6958bc6b66ff0ce85aaaa5
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-12-05 20:06:53 -0600

    started methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,25 @@
+public class MethodsExercises {
+
+    public static String addition() {
+
+    }
+
+    public static String subtraction() {
+
+    }
+
+    public static String multiplication() {
+
+    }
+
+    public static String division() {
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
