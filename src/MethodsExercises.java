2d4893a4081deb9705ea423e6becc89c6a280a99
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-12-07 15:47:06 -0600

    methods exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,25 +1,50 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
-
-    public static String addition() {
-
-    }
-
-    public static String subtraction() {
-
-    }
-
-    public static String multiplication() {
-
-    }
-
-    public static String division() {
-
-    }
-
     public static void main(String[] args) {
 
-
+        System.out.println(addition(2.5, 2.5));
+        System.out.println(subtraction(2, 2));
+        System.out.println(multiplication(2, 2));
+        System.out.println(division(2, 10));
+        System.out.println(modulus(100, 2));
 
     }
+    public static double addition(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double division(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public static double modulus(double num1, double num2) {
+        return num1 % num2;
+    }
+
+    public static int getInteger(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) {
+            System.out.println("Not a number!");
+            return getInteger(min, max);
+        }
+        int userInput = sc.nextInt();
+        if (userInput >= min && userInput <= max) {
+            return userInput;
+        } else {
+            System.out.println("Number not in range!");
+            return getInteger(min, max);
+        }
+    }
+
+
 
 }
