a3a9ec7b31f8b6d20fe6e64da74ad6a6bf5bda48
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-12-12 09:05:05 -0600

    working on array exercises java

---
+++ src/ArraysExercises.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class ArraysExercises extends Person {
+
+    public ArraysExercises(String name) {
+        super(name);
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+        String[] Person = {"Luke", "Han", "Leia"};
+
+        for (int i = 0; i < Person.length; i += 1) {
+            System.out.println(Person[i]);
+        }
+    }
+
+    public static addPerson()
+
+}
