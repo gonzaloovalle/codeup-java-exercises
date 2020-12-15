e53b883c8f361ac5b19658fa18accd13315f40a6
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-12-15 15:38:32 -0600

    started interfaces exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,42 @@
 package shapes;
 
-public class Rectangle {
-    protected double length;
-    protected double width;
-
-    public Rectangle(double l, double w){
-        this.length = l;
-        this.width = w;
+public class Rectangle extends Quadrilateral implements Measurable{
+    public Rectangle(double l, double w) {
+        super(l, w);
     }
 
-    public double getArea(){
-        return length * width;
+    @Override
+    public void setLength(double length) {
+
     }
 
-    public double getPerimeter(){
-        return (2 * length) + (2 * width);
+    @Override
+    public void setWidth(double width) {
+
     }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle(double l, double w){
+//        this.length = l;
+//        this.width = w;
+//    }
+//
+//    public double getArea(){
+//        return length * width;
+//    }
+//
+//    public double getPerimeter(){
+//        return (2 * length) + (2 * width);
+//    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,15 +1,38 @@
 package shapes;
 
-public class Square extends Rectangle {
-    public Square(double side){
-        super(side, side);
+public class Square extends Quadrilateral{
+    public Square(double l, double w) {
+        super(l, w);
     }
 
-    public double getArea(){
-        return length * length;
+    @Override
+    public void setLength(double length) {
+
     }
 
-    public double getPerimeter(){
-        return 4 * length;
+    @Override
+    public void setWidth(double width) {
+
     }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+//    public Square(double side){
+//        super(side, side);
+//    }
+//
+//    public double getArea(){
+//        return length * length;
+//    }
+//
+//    public double getPerimeter(){
+//        return 4 * length;
+//    }
 }
