d29b53a005bbb2815e9a8a58bbc82133cda418b3
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-12-18 09:14:23 -0600

    collections exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -4,18 +4,31 @@ import java.util.ArrayList;
 
 public class Student {
     private String name;
-    private ArrayList<Integer> grade = new ArrayList<>();
+    private ArrayList<Integer> grades;
 
-    public Student(String name, Integer grade){
+    public Student(String name){
+        this.name = name;
+        this.grades = new ArrayList<>();
 
     }
 
     // returns the student's name
-    public String getName();
+    public String getName(){
+        return name;
+    }
     // adds the given grade to the grades property
-    public void addGrade(int grade);
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    };
     // returns the average of the students grades
-    public double getGradeAverage();
+    public double getGradeAverage(){
+        int total = 0;
+        for (int grade : grades){
+            total += grade;
+        }
+
+        return (double) total
+    };
 
     public static void main(String[] args) {
         Student student1 = new Student("Gonzalo", 95);
