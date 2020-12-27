b40810072997c24e485cc48cb9c649f42bb04bf5
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-12-27 06:46:56 -0600

    completed arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,22 +1,31 @@
 import java.util.Arrays;
 
-public class ArraysExercises extends Person {
-
-    public ArraysExercises(String name) {
-        super(name);
-    }
-
+public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
 
-        String[] Person = {"Luke", "Han", "Leia"};
+        Person[] people = new Person[3];
+        people[0] = new Person("Luke");
+        people[1] = new Person("Leia");
+        people[2] = new Person("Han");
 
-        for (int i = 0; i < Person.length; i += 1) {
-            System.out.println(Person[i]);
+        for(Person person : people) {
+            System.out.println(person.getName());
+        }
+
+        Person[] newArray = addPerson(people, new Person("Lando"));
+
+        for(Person person : newArray) {
+            System.out.println(person.getName());
         }
     }
 
-//    public static addPerson()
+    public static Person[] addPerson(Person[] pplArray, Person personToAdd) {
+        Person[] tempArray = Arrays.copyOf(pplArray, pplArray.length+1);
+        tempArray[tempArray.length-1] = personToAdd;
+
+        return tempArray;
+    }
 
 }
